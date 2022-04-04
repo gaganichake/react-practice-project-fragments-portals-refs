64d705a2863435d3821ab9b9a0b41638013fb2c9
Gagan Ichake <gaganichake@yahoo.com>
2022-04-04 11:17:46 -0500

    Adding a re-usable "Button" Component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,15 @@
+import classses from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <button
+      className={classses.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,4 @@
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -13,7 +14,7 @@ const AddUser = () => {
         <input id="username" type="text" maxLength={20}></input>
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" maxLength={2}></input>
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
